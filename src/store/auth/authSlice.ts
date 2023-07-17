a35dd93b1bcd75d89f0004d92fcc3921d65c385b
강다연 <dksldiss89@gmail.com>
2023-07-17 16:17:24 +0900

    회원가입 reducer, type

--- src/store/auth/authSlice.ts
+++ src/store/auth/authSlice.ts
@@ -4,7 +4,7 @@ import type {PayloadAction} from "@reduxjs/toolkit";
 // ** Redux **
 import {PURGE} from "redux-persist";
 // ** Type **
-import {AuthState, ILogin} from "./type";
+import {AuthState, ILogin, ISignUp} from "./type";
 // ** Data **
 import {users} from "../../database/users";
 
@@ -32,6 +32,15 @@ const AuthSlice = createSlice({
         },
         logout: (state) => {
             state.isLogin = false
+        },
+        signup: (state, action: PayloadAction<ISignUp>) => {
+            state.userList.push({
+                id: (state.userList.length === 0) ? 1 : Math.max(...state.userList.map(user => user.id)) + 1,
+                name: action.payload.name,
+                loginId: action.payload.loginId,
+                password: action.payload.password,
+                todos: []
+            })
         }
     },
     extraReducers: (builder) => {
@@ -39,5 +48,5 @@ const AuthSlice = createSlice({
     }
 })
 
-export const {login,logout} = AuthSlice.actions
+export const {login,logout,signup} = AuthSlice.actions
 export default AuthSlice.reducer;
--- src/store/auth/type.ts
+++ src/store/auth/type.ts
@@ -1,8 +1,11 @@
+import {TodosType} from "../todos/type";
+
 export interface IUser {
     id: number,
     name: string,
     loginId: string,
     password: string,
+    todos: TodosType
 }
 
 export type userListType = IUser[]
@@ -10,10 +13,16 @@ export type userListType = IUser[]
 export interface AuthState {
     userList: userListType,
     loginUser?: IUser,
-    isLogin: boolean
+    isLogin: boolean,
 }
 
 export interface ILogin {
+    loginId: string,
+    password: string
+}
+
+export interface ISignUp {
+    name: string,
     loginId: string,
     password: string
 }
