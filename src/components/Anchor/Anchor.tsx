8d1b7985125cabdf13450daebcbf63b7672b7042
강다연 <dksldiss89@gmail.com>
2023-07-17 16:17:39 +0900

    component추가

---
+++ src/components/Anchor/Anchor.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {styled} from "styled-components";
+import {Link} from "react-router-dom";
+
+// ** props interface **
+interface props {
+    link: string,
+    children: string
+}
+
+// ** Styled-Component **
+const LinkWrap = styled.div`
+    margin-top: 10px;
+    text-align: center;
+`
+const StyledLink = styled(Link)`
+    font-size: 12px;
+    color: #3d79b1;
+`
+
+function Anchor(props: props){
+    const children = props.children
+    const link = props.link
+    return (
+        <LinkWrap>
+            <StyledLink to={link}>
+                {children}
+            </StyledLink>
+        </LinkWrap>
+    )
+}
+
+export default Anchor
