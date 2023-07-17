89e8428d18dd1ade8e4574286b6259f06a422235
강다연 <dksldiss89@gmail.com>
2023-07-17 16:18:03 +0900

    상태에 따른 페이지 변화

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+// ** Hook **
+import {useAppSelector} from "../../hooks/TypedUseSelector";
 // ** Component **
 import TodoInsert from "../../components/Todo/TodoInsert";
 import TodoList from "../../components/Todo/TodoList";
 import LayoutWrapper from "../../layout/LayoutWrapper";
 import Header from "../../layout/Header";
 import Content from "../../layout/Content";
+import {useNavigate} from "react-router-dom";
 
 function Main() {
+    const isLogin = useAppSelector(state => state.auth.isLogin)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        isLogin ? navigate('/') : navigate('/login')
+    },[])
+
     return (
         <LayoutWrapper>
             <Header></Header>
