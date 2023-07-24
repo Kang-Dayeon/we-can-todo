69aec5c1f6bef4f66aa710b2853ec56a4a3a3761
강다연 <dksldiss89@gmail.com>
2023-07-24 13:51:53 +0900

    코드 정리

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import {Link} from "react-router-dom";
 // ** Redux **
 import {useDispatch} from "react-redux";
 import {login} from "../../store/auth/authSlice";
