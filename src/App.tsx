4a2cd41926af3fb7493c6876be1e21dac0bdf91f
강다연 <dksldiss89@gmail.com>
2023-07-25 11:27:04 +0900

    axios 연결 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Routers from "./Router";
 // ** redux **
 import {Provider} from "react-redux";
 import {store, persistor} from "./store/store";
 import {BrowserRouter} from "react-router-dom";
 import {PersistGate} from "redux-persist/integration/react";
+// ** axios **
+import axios from 'axios';
 
 function App() {
+    const sendRequest = async () => {
+        const response = await axios.get('http://localhost:8080')
+        console.log(response)
+        console.log(response.data)
+    }
+
+    useEffect(() => {
+        sendRequest()
+    })
   return (
       <BrowserRouter>
         <Provider store={store}>
