09669384d1659c557503f781357a4b475946350d
강다연 <dksldiss89@gmail.com>
2023-07-26 11:51:30 +0900

    mysql 설치

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
+require('dotenv').config()
 // ** server **
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
 const app = express()
+
+// ** Data **
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_ID,
+  password: process.env.DB_PW,
+  database: 'weCanTodo'
+})
+
+connection.connect();
+
+connection.query('SELECT * form users', (error, rows, fields) => {
+  if(error) throw error;
+  console.log('user info is : ', rows)
+})
+
+connection.end();
 
 const server = require('http').createServer(app)
 
