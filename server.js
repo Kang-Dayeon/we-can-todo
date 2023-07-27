2d96670be5186a85b5d4baa760a82fe93a795a0b
강다연 <dksldiss89@gmail.com>
2023-07-27 15:38:09 +0900

    login post요청 받기

--- server.js
+++ server.js
@@ -3,21 +3,31 @@ require('dotenv').config()
 const express = require('express')
 const multer = require('multer')
 const fs = require('fs')
-const bodyPerser = require('body-parser')
+const bodyParser = require('body-parser')
 const db = require('./config/mysql')
 const path = require('path')
 const cors = require('cors')
 
 const app = express()
+app.use(cors({credentials: true, origin: "http://localhost:3000"})) // cors 미들웨어
+
 const conn = db.init()
 
 const server = require('http').createServer(app)
 
 // ** 미들웨어 **
-app.use(bodyPerser.json())
-app.use(bodyPerser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(cors()) // cors 미들웨어
+app.post('/api/login', (req, res) => {
+  let userId = req.body.loginId
+  let password = req.body.password
+  const sql = "SELECT * FROM users WHERE userId=? AND password=?"
+  conn.query(sql,[userId,password], function(err, result){
+    if(err) throw console.log("query is not excuted: " + err)
+    else res.send(result)
+  })
+})
 
 app.get('/', (req, res) => {
   const sql = "select * from todoList"
