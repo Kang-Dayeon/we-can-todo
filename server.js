1e0c8462ac29f839b5f2017d0aa5cb133fec245a
강다연 <dksldiss89@gmail.com>
2023-07-28 16:42:33 +0900

    express session 설치

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 // ** library **
 const express = require('express')
+const expressSession = require('express-session')
 const multer = require('multer')
 const fs = require('fs')
 const bodyParser = require('body-parser')
@@ -9,7 +10,7 @@ const path = require('path')
 const cors = require('cors')
 
 const app = express()
-app.use(cors({credentials: true, origin: "http://localhost:3000"})) // cors 미들웨어
+app.use(cors({credentials: true, origin: "*"})) // cors 미들웨어
 
 const conn = db.init()
 
@@ -19,21 +20,45 @@ const server = require('http').createServer(app)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+// ** session **
+app.use(
+  expressSession({
+    secret: "my key",
+    resave: true,
+    saveUninitialized: true
+  })
+)
+
+app.get('/', (req, res) => {
+  if(req.session){
+    res.redirect('/')
+  } else {
+    res.redirect('/login')
+    const sql = "select * from todoList"
+    conn.query(sql, function(err, result){
+      if(err) console.log("query is not excuted: " + err)
+      else res.send(result)
+    })
+  }
+})
+
 app.post('/api/login', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*")
   let userId = req.body.loginId
   let password = req.body.password
   const sql = "SELECT * FROM users WHERE userId=? AND password=?"
   conn.query(sql,[userId,password], function(err, result){
-    if(err) throw console.log("query is not excuted: " + err)
-    else res.send(result)
-  })
-})
-
-app.get('/', (req, res) => {
-  const sql = "select * from todoList"
-  conn.query(sql, function(err, result){
-    if(err) console.log("query is not excuted: " + err)
-    else res.send(result)
+    if(err) {
+      throw console.log('query is not excuted: ' + err)
+    } else {
+      res.session = {
+        id: result[0].id,
+        userId: result[0].userId,
+        pw: result[0].password,
+        name: result[0].name,
+        authorized: true,
+      }
+    }
   })
 })
 
